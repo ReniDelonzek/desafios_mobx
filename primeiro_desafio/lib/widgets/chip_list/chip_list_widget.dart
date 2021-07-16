77ac34268921f9a5e172bf691a5195ac9a88276d
filipemendes18 <filipem0512@gmail.com>
2021-07-15 21:23:10 -0300

    Tela inicial criada

--- primeiro_desafio/lib/widgets/chip_list/chip_list_widget.dart
+++ primeiro_desafio/lib/widgets/chip_list/chip_list_widget.dart
@@ -13,7 +13,46 @@ class ChipList extends StatelessWidget {
             Container(
               color: Colors.green.shade600,
               child: Row(
-                children: [],
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    color: Colors.green.shade800,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 20),
+                          child: Text(
+                            "Lista 1",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.add, color: Colors.white),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.add, color: Colors.white),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.add, color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 60)
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
